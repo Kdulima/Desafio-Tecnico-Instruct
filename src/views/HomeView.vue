7682d6760d9f5b92776b6b3377ab6e1d3e3327cf
carloslima <carlos.eduardo@the9level.com>
2022-05-23 17:26:36 -0300

    feat(create): Implementation carousel idea

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,26 +3,83 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <p>Habitue</p>
   </div>
+   <!-- <div class="app">
+      <carousel >
+          <carousel-slide v-for="slide in slides" :key="slide" class="carousel-slider">
+              <img :src="slide" :alt="slide">
+          </carousel-slide>
+      </carousel>
+    </div> -->
 </template>
 
 <style>
 
-.home {
+  .home {
   height: 80vh;
   display: flex;
   justify-content: center;
   align-items: center;
-}
+  }
 
-.home p{
-  font-size: 200px;
-  font-weight: bold;
-  color: black;
-}
+  .home p{
+    font-size: 200px;
+    font-weight: bold;
+    color: black;
+  }
+
+  /* .app {
+      display:flex;
+      justify-content: center;
+  }
+  .carousel {
+      position:relative;
+      overflow: hidden;
+      width:800px;
+      height:500px;
+      z-index:10;
+  }
+  .btn {
+      padding:5px 10px;
+      background-color:rgba(0,0,0,0.5);
+      border:1px solid transparent;
+      margin:5px 10px;
+      color:#FFF;
+      height:50px;
+      width:50px;
+      position:absolute;
+      margin-top:-25px;
+      z-index:2;
+  }
+  .btn:hover {
+      cursor: pointer;
+  }
+  .btn:focus{
+      outline:none;
+  }
+  .btn-next {
+      top:50%;
+      right:0;
+  }
+  .btn-prev {
+      top:50%;
+      left:0;
+  }
+  .carousel-slider {
+      position:absolute;
+      top:0;
+      left:0;
+      bottom:0;
+      right:0;
+  }
+  .carousel-slider img {
+      width:100%;
+      height:100%;
+  } */
+
 </style>
 
 <script>
-// @ is an alias to /src
+
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
@@ -31,4 +88,28 @@ export default {
     HelloWorld,
   },
 };
+
+// import Carousel from '../components/carousel/CarouselHome.vue';
+// import CarouselSlide from '../components/carousel/CarouselSlide.vue';
+
+// export default {
+//   data() {
+//     return {
+//       slides: [
+//         'https://picsum.photos/id/230/600/300',
+//         'https://picsum.photos/id/231/600/300',
+//         'https://picsum.photos/id/232/600/300',
+//         'https://picsum.photos/id/233/600/300',
+//         'https://picsum.photos/id/234/600/300',
+//         'https://picsum.photos/id/235/600/300',
+//         'https://picsum.photos/id/236/600/300',
+//       ],
+//     };
+//   },
+//   components: {
+//     Carousel,
+//     CarouselSlide,
+//   },
+// };
+
 </script>
