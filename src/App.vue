0857c7bb986c52e191ecec59b4589ddab6be2919
carloslima <carlos.eduardo@the9level.com>
2022-05-23 10:39:48 -0300

    feat(create): Implementation of filter buttons

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/main">Filtros</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
